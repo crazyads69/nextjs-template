d2114f1e971ca5a3d46977068c425738ea491ba7
Lê Minh Trí <leminhtri11a1@gmail.com>
2024-07-15 09:44:47 +0700

    feat: Add ESLint and Prettier configurations
    Adds ESLint and Prettier configurations to the project to enforce code style and catch potential errors.
    
    - ESLint config is based on Airbnb style guide and Next.js recommendations.
    - Prettier config sets up consistent code formatting.
    - Husky and lint-staged are used to automatically run linting and formatting on pre-commit.
    
    Authored-by: Lê Minh Trí <leminhtri11a1@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.node$/,
+            use: "file-loader",
+        });
+
+        return config;
+    },
+    images: {
+        domains: ["localhost"],
+    },
+    reactStrictMode: false,
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    output: "standalone",
+};
 
 export default nextConfig;
